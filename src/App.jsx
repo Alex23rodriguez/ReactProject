e0bb2847060c9fd19d7e8dbec735acf31e9df91c
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2021-06-01 16:49:51 -0500

    added routing to menu-item with withRoute HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
